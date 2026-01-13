444931a0629d1625282b5b6cea08e4554c62e1bd
MARIEM <mariemcharef42@gmail.com>
2026-01-13 18:24:52 +0100

    add user login functionality

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,5 +3,5 @@ import { registerUser, loginUser } from '../controllers/user.controller.js';
 
 const router = Router();
 router.route('/register').post(registerUser);
-
+router.route('/login').post(loginUser);
 export default router;
